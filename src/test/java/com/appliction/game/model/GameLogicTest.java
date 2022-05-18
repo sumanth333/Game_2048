1a1fd7c2dcfade40a12f51eb78defe791164c885
sumanth333 <polesumanth333@gmail.com>
2022-05-19 00:56:42 +0530

    [Sumanth] Introduce GameLogic

---
+++ src/test/java/com/appliction/game/model/GameLogicTest.java
@@ -0,0 +1,32 @@
+package com.appliction.game.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GameLogicTest {
+    @BeforeEach
+    void setUp() {
+        GameLogic.getInstance().resetScoreOfTilesMove();
+    }
+
+    @Test
+    void shouldAddScoreOnMergingTiles() {
+        GameLogic gameLogic = GameLogic.getInstance();
+        assertEquals(0, gameLogic.getScoreOfTilesMove());
+
+        gameLogic.addScoreOnMergingTiles(8);
+        assertEquals(8, gameLogic.getScoreOfTilesMove());
+    }
+    @Test
+    void shouldResetMergeScoreAfterAddingToScoreBoard() {
+        GameLogic gameLogic = GameLogic.getInstance();
+        gameLogic.addScoreOnMergingTiles(10);
+        assertEquals(10, gameLogic.getScoreOfTilesMove());
+
+        gameLogic.resetScoreOfTilesMove();
+        assertEquals(0, gameLogic.getScoreOfTilesMove());
+    }
+
+}
